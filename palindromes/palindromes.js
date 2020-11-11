baf65d9f9563924334aac19a1b45947ffc5f7722
josegarrera <josegarrera@gmail.com>
2020-11-11 09:27:51 -0300

    Palindromes.js ready.

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function() {
-
+const palindromes = function(s) {
+    let string = s.toLowerCase();
+    string = string.replace(/[^a-z]/g, '');
+    let array = string.split('');
+    array = array.reverse();
+    let reverseString = array.join('');
+    return string === reverseString;
 }
 
 module.exports = palindromes
