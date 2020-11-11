63dd922c9180782787fc6fe67e590aab4bfa6da5
josegarrera <josegarrera@gmail.com>
2020-11-11 19:59:43 -0300

    fibonacci.js tested ok

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) {
+        return 'OOPS';
+    } else if (index === 1 || index === 2) {
+        return 1;
+    } else {
+        let fibonacciArray = [1, 1];
+        for (let i = 2; i <= index; i++) {
+            fibonacciArray.push(fibonacciArray[i-2] + fibonacciArray[i-1]);
+        }
+        return fibonacciArray[index - 1];
+    }
 }
 
 module.exports = fibonacci
