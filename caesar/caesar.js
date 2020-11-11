93ff7891ca54aa89ed304547829969e97c58012c
josegarrera <josegarrera@gmail.com>
2020-11-11 14:04:12 -0300

    caesar.js ready to test.

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,45 @@
-const caesar = function() {
-
+const caesar = function(message, shift) {
+    let upperCaseDecimalUnicode = [];
+    let upperCode = 65;
+    for (let i = 0; i <= 25; i++) {
+        upperCaseDecimalUnicode[i] = upperCode;
+        upperCode++;
+    }
+    let lowerCaseDecimalUnicode = [];
+    let lowerCode = 97;
+    for (let j = 0; j <= 25; j++) {
+        lowerCaseDecimalUnicode[j] = lowerCode;
+        lowerCode++;
+    }
+    let stringUnicodeArray = [];
+    for (let l = 0; l < message.length; l++) {
+        stringUnicodeArray[l] = message.charCodeAt(l);
+    }
+    shift = shift % upperCaseDecimalUnicode.length;
+    for (let k = 0; k < stringUnicodeArray.length; k++) {
+        if (upperCaseDecimalUnicode.includes(stringUnicodeArray[k])) {
+            if (stringUnicodeArray[k] + shift > 90) {
+                stringUnicodeArray[k] = stringUnicodeArray[k] + shift - 90 + 64;
+            } else if (stringUnicodeArray[k] + shift < 65) {
+                stringUnicodeArray[k] = stringUnicodeArray[k] - 64 + 90 + shift;
+            } else {
+            stringUnicodeArray[k] = stringUnicodeArray[k] + shift;
+            }
+        }
+        if (lowerCaseDecimalUnicode.includes(stringUnicodeArray[k])) {
+            if (stringUnicodeArray[k] + shift > 122) {
+                stringUnicodeArray[k] = stringUnicodeArray[k] + shift - 122 + 96;
+            } else if (stringUnicodeArray[k] + shift < 97) {
+                stringUnicodeArray[k] = stringUnicodeArray[k] - 96 + 122 + shift;
+            } else {
+            stringUnicodeArray[k] = stringUnicodeArray[k] + shift;
+            }
+        }
+    }
+    for (let m = 0; m < stringUnicodeArray.length; m++) {
+        stringUnicodeArray[m] = String.fromCharCode(stringUnicodeArray[m]);
+    }
+    return stringUnicodeArray.join('');
 }
 
 module.exports = caesar
