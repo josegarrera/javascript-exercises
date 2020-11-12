b8c300f461fd89bc73c12f3f9a2215e20136aa25
josegarrera <josegarrera@gmail.com>
2020-11-12 16:01:48 -0300

    findTheOldest.js tested ok

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,36 @@
-let findTheOldest = function() {
+let findTheOldest = function(array) {
+    let people = array;
+    for (let i = 0; i < people.length; i++) {
+        if (!('yearOfDeath' in people[i])) {
+            people[i] = {
+                name: people[i]['name'],
+                yearOfBirth: people[i]['yearOfBirth'],
+                yearOfDeath: new Date().getFullYear()
+        };
+        }
+    }
+    const oldestPeople = people.sort(function(a,b) {
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        if (lastPerson > nextPerson) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+    return people[0];
 
-}
 
-module.exports = findTheOldest
+    // let total = 0;
+    // let oldestPerson = {};
+    // people.reduce((prev, curr) => {
+    //     if ((curr.yearOfDeath - curr.yearOfBirth) > total) {
+    //         total = curr.yearOfDeath - curr.yearOfBirth;
+    //         oldestPerson = curr;
+    //     }
+    //     return prev;
+    // })
+    // return oldestPerson;
+}; 
+
+module.exports = findTheOldest;
